e9ffb5bee17044a6e2166bc2da2f0420aea60d13
chirag <chiragjain78382@gmail.com>
2024-10-16 02:21:54 +0530

    init

---
+++ app/Room.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider publicApiKey={"pk_dev_DFHeWfci1JL3p1U4Y0k9agMCCr8bGfeOO2HXVfIh3OpZWpE-Ki8p4qCoPpYPZinM"}>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
